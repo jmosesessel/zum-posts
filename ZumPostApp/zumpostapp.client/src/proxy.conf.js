31b22a1f37d682ed9c8124cb0654798fc8deb862
jmosesessel <jmosesessel21@gmail.com>
2024-02-17 13:24:56 -0500

    disable proxy config by commenting code out

--- ZumPostApp/zumpostapp.client/src/proxy.conf.js
+++ ZumPostApp/zumpostapp.client/src/proxy.conf.js
@@ -1,23 +1,23 @@
-const { env } = require('process');
+// const { env } = require('process');
 
-const target = env.ASPNETCORE_HTTPS_PORT ? `https://localhost:${env.ASPNETCORE_HTTPS_PORT}` :
-    env.ASPNETCORE_URLS ? env.ASPNETCORE_URLS.split(';')[0] : 'https://localhost:7037';
+// const target = env.ASPNETCORE_HTTPS_PORT ? `https://localhost:${env.ASPNETCORE_HTTPS_PORT}` :
+//     env.ASPNETCORE_URLS ? env.ASPNETCORE_URLS.split(';')[0] : 'https://localhost:7037';
 
-const PROXY_CONFIG = [
-  {
-    context: [
-      "/weatherforecast",
-    ],
-    target,
-    secure: false
-  },
-  //{
-  //  context: [
-  //    "https://localhost:7037/api/posts",
-  //  ],
-  //  target,
-  //  secure: false
-  //}
-]
+// const PROXY_CONFIG = [
+//   // {
+//   //   context: [
+//   //     "/weatherforecast",
+//   //   ],
+//   //   target,
+//   //   secure: false
+//   // },
+//   //{
+//   //  context: [
+//   //    "https://localhost:7037/api/posts",
+//   //  ],
+//   //  target,
+//   //  secure: false
+//   //}
+// ]
 
-module.exports = PROXY_CONFIG;
+// module.exports = PROXY_CONFIG;
