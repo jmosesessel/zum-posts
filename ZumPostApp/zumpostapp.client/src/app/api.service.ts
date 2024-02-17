574a189418f65eec397118ea0b8c9d80bc072ba6
jmosesessel <jmosesessel21@gmail.com>
2024-02-17 09:49:53 -0500

    create a service to handle the http requests to the backend

--- ZumPostApp/zumpostapp.client/src/app/api.service.ts
+++ ZumPostApp/zumpostapp.client/src/app/api.service.ts
@@ -7,12 +7,13 @@ import { Post } from './post';
   providedIn: 'root'
 })
 export class ApiService {
-  private apiUrl = 'https://localhost:7037/api/Posts'; // Adjust the API URL accordingly
+  private apiUrl = 'https://localhost:7037/api/Posts'; 
 
   constructor(private http: HttpClient) {
     console.log('http', http.request)
   }
 
+  // function to get all posts filtered by the tag input from the users
   getPosts(tag: string): Observable<Post[]> {
     const posts = this.http.get<Post[]>(`${this.apiUrl}/${tag}`);
     console.log('posts', posts)
